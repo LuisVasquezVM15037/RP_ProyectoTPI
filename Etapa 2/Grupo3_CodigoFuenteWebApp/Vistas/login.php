34b740b12d7a76ec2aa7d2c57f9e5d00bfe98021
JosueUES <149845859+JosueUES@users.noreply.github.com>
2025-10-20 18:03:15 -0600

    Update login.php

--- Etapa 2/Grupo3_CodigoFuenteWebApp/Vistas/login.php
+++ Etapa 2/Grupo3_CodigoFuenteWebApp/Vistas/login.php
@@ -21,7 +21,10 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 <head>
     <meta charset="UTF-8">
     <title>Login</title>
-<link rel="stylesheet" href="CSS/login.css"><body>
+    <!-- ✅ Ruta corregida -->
+    <link rel="stylesheet" href="../CSS/login.css">
+</head>
+<body>
     <div class="header">
         <div>INICIO</div>
         <div>REGISTRO</div>
