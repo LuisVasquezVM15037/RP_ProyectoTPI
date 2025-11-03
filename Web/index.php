57e521e28c704f16f2fa05b69aa5e1d080e29678
LuisVasquezVM15037 <vm15037@ues.edu.sv>
2025-11-02 22:12:04 -0600

    Revert "dsds"
    This reverts commit 9b29c1bf73b1dbedfd2eaf4ac61c48a164984b70.

--- Web/index.php
+++ Web/index.php
@@ -5,7 +5,7 @@ if (isset($_SESSION[""]) && $_SESSION[""]   == "") {
     header("Location: index.php");
 } 
 
-define('BASE_URL', 'http://localhost/Grupo3_CodigoFuenteAppWeb/'); //Esta url se ajusta según la configuración del servidor
+define('BASE_URL', 'http://localhost/Web/'); //Esta url se ajusta según la configuración del servidor
 
 // Cargar la configuración de PayPal (asegurar que las constantes PAYPAL_* estén disponibles)
 require_once __DIR__ . '/config/paypal.php';
